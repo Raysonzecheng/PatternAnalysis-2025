abc226bb5de30cd554f9d40f78905f79803a4e14
Raysonzecheng <s4912814@student.uq.edu.au>
2025-10-30 19:32:43 +0800

    Refactor dataset module for modular design and add class-balanced oversampling function

--- recognition/siamese_s49128145/dataset.py
+++ recognition/siamese_s49128145/dataset.py
@@ -1,38 +1,68 @@
-import torch
-from torch.utils.data import Dataset
-from torchvision import transforms
-from PIL import Image
 import os
 import random
+import numpy as np
 import pandas as pd
+import torch
+import cv2
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from PIL import Image, ImageEnhance, ImageOps, ImageFilter
+import random
+import os
 
-class ISICDataset(Dataset):
-    def __init__(self, csv_file, image_folder, transform=None):
-        self.data = pd.read_csv(csv_file)
-        self.image_folder = image_folder
-        self.transform = transform or transforms.Compose([
-            transforms.Resize((224, 224)),
-            transforms.ToTensor()
-        ])
-        self.label_map = { 'melanoma': 1, 'normal': 0 }
 
-    def __getitem__(self, idx):
-        img1_path = os.path.join(self.image_folder, self.data.iloc[idx, 0])
-        label1 = self.label_map[self.data.iloc[idx, 1]]
-
-        # 随机选一个同类或不同类图片
-        should_match = random.randint(0, 1)
-        while True:
-            idx2 = random.randint(0, len(self.data) - 1)
-            label2 = self.label_map[self.data.iloc[idx2, 1]]
-            if (should_match and label1 == label2) or (not should_match and label1 != label2):
-                break
-
-        img2_path = os.path.join(self.image_folder, self.data.iloc[idx2, 0])
-        img1, img2 = Image.open(img1_path).convert('RGB'), Image.open(img2_path).convert('RGB')
-        if self.transform:
-            img1, img2 = self.transform(img1), self.transform(img2)
-        return img1, img2, torch.tensor(float(label1 != label2), dtype=torch.float32)
+###############################################################################
+### Classes
+class ClassificationDataset(torch.utils.data.Dataset):
+    """
+    Standard dataset generator for binary classification.
+    Each item returns (image, label).
+    """
+    def __init__(self, images, labels, transform=None):
+        self.images = images
+        self.labels = labels
+        self.transform = transform
 
     def __len__(self):
-        return len(self.data)
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        img = cv2.imread(self.images[idx]) / 255.0
+        label = self.labels[idx]
+
+        if self.transform:
+            img = self.transform(img)
+
+        return img, torch.tensor(label, dtype=torch.long)
+
+def augment_image(img_path):
+    """对单张图像执行简单的数据增强并返回新图像路径"""
+    img = Image.open(img_path).convert('RGB')
+
+    # 随机翻转
+    if random.random() > 0.5:
+        img = ImageOps.mirror(img)
+    if random.random() > 0.5:
+        img = ImageOps.flip(img)
+
+    # 随机旋转（±20°）
+    angle = random.uniform(-20, 20)
+    img = img.rotate(angle)
+
+    # 随机亮度调整
+    enhancer = ImageEnhance.Brightness(img)
+    img = enhancer.enhance(random.uniform(0.8, 1.2))
+
+    # 随机模糊
+    if random.random() > 0.7:
+        img = img.filter(ImageFilter.GaussianBlur(radius=random.uniform(0, 1.0)))
+
+    # 保存到同目录下的新文件
+    dirname = os.path.dirname(img_path)
+    basename = os.path.basename(img_path)
+    name, ext = os.path.splitext(basename)
+    new_path = os.path.join(dirname, f"{name}_aug{random.randint(1000,9999)}{ext}")
+    img.save(new_path)
+
+    return new_path
